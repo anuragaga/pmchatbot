947856ab946b2ddef10684082e590ec4fbe2df2f
anuragaga <33158891+anuragaga@users.noreply.github.com>
2025-04-30 18:50:49 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,6 +4,35 @@ from utils import write_message
 # from PMagent import generate_response
 from PMAgentAzure import generate_response
 
+# Create secrects.toml file
+
+import os
+
+# Dynamically create the secrets.toml file
+secrets_content = f"""
+[general]
+
+AZURE_CLIENT_ID = "{os.getenv('AZURE_CLIENT_ID')}"
+AZURE_CLIENT_SECRET = "{os.getenv('AZURE_CLIENT_SECRET')}"
+AZURE_CISCO_OPENAI_APP_KEY = "{os.getenv('AZURE_CISCO_OPENAI_APP_KEY)}"
+CISCO_BRAIN_USER_ID = "{os.getenv('CISCO_BRAIN_USER_ID')}"
+URL = "{os.getenv('URL')}"
+AZURE_ENDPOINT = "{os.getenv('AZURE_ENDPOINT')}"
+OPENAI_API_KEY = "{os.getenv('OPENAI_API_KEY')}"
+NEO4J_URI = "{os.getenv('NEO4J_URI')}"
+NEO4J_USERNAME = "{os.getenv('NEO4J_USERNAME')}"
+NEO4J_PASSWORD = "{os.getenv('NEO4J_PASSWORD')}"
+
+"""
+
+# Write the content to the secrets.toml file
+with open("/root/.streamlit/secrets.toml", "w") as secrets_file:
+    secrets_file.write(secrets_content)
+
+
+
+
+
 # Page Config
 st.set_page_config("ProjectManager", page_icon=":material/monitoring:")
 
